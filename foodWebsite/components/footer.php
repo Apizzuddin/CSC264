82d99fac54e8fc88e44c3ecabb1d1b02ae2fdfd3
Apizzuddin <138226016+Apizzuddin@users.noreply.github.com>
2023-06-30 22:09:42 +0800

    Add files via upload

---
+++ foodWebsite/components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="zamir@gmail.com">zamir@gmail.com</a>
+         <a href="joemick@gmail.com">joemick@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>06:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Kuching, Sarawak -93000</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">014-666-9910</a>
+         <a href="tel:1112223333">019-717-8199</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
